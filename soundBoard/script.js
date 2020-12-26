dc48ccbedfc60d469d5d3357201d3e07f47c6b15
Jo Dunham <scaffelpike@yahoo.com.au>
2020-12-26 15:23:43 +1100

    sound board created

---
+++ soundBoard/script.js
@@ -0,0 +1,24 @@
+const sounds = ['babyLaugh','burp', 'clapping', 'drip', 'pig', 'punch', 'wave'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    })
+}
