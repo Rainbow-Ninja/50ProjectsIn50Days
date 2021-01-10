de79b7b9a54f8e47aacf3a81a3ead24da139ae0f
Jo Dunham <scaffelpike@yahoo.com.au>
2021-01-10 16:28:40 +1100

    js for small cups added for filling when clicked

---
+++ drinkWater/script.js
@@ -0,0 +1,26 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const litres = document.getElementById('litres')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+})
+
+function highlightCups(idx) {
+    // if(smallCups[idx].classList.contains('full') && !smallCups[idx+1].classList.contains('full')) {
+    //     idx--
+    // }
+    //these two things so the same thing! Just fancier way of writing it I guess
+    //checking if the one I'm clicking is full and the next one is empty, then decrease full by one
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+    smallCups.forEach((cup, idx2) => {
+        if(idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+}
